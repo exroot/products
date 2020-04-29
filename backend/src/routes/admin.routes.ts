0d670d66cb421873c27fefdb18304452147fd9fa
exroot <eliastorrealba1998@outlook.com>
2020-04-29 17:22:33 -0400

    Deleting admin routes, at least by now

--- backend/src/routes/admin.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const router: Router = Router();
-
-router.get("/api/v2/", function (req, res, next) {
-    res.status(200).json({ msg: "Hi admin" });
-});
-
-export default router;
