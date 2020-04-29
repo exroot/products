aa7e649de042565ae332d767279af9005b0e8573
exroot <eliastorrealba1998@outlook.com>
2020-04-29 17:25:13 -0400

    Deleting shop routes, it could be too extensive

--- backend/src/routes/shop.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const router: Router = Router();
-
-router.get("/api/v1/", function (req, res, next) {
-    res.status(200).json({ msg: "Hi shop" });
-});
-
-export default router;
