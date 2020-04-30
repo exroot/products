a056fe19d87dc04c923c76d23ec9b77e19ba929a
exroot <eliastorrealba1998@outlook.com>
2020-04-30 13:01:45 -0400

    method's names are now more specific

--- backend/src/controllers/battery.controller.ts
+++ backend/src/controllers/battery.controller.ts
@@ -1,10 +1,11 @@
 import { Request, Response, NextFunction } from "express";
 import {
-    findAll,
-    findOne,
-    create,
-    update,
-    remove,
+    findAllBatteries,
+    findBattery,
+    createBattery,
+    updateBattery,
+    removeBattery,
+    IBattery,
 } from "../services/battery.service";
 import { Battery } from "../entity";
 
@@ -14,7 +15,7 @@ export const getAllBatteries = async (
     next: NextFunction
 ): Promise<Response | void> => {
     try {
-        const batteries = await findAll();
+        const batteries = await findAllBatteries();
         return res.status(200).json(batteries);
     } catch (err) {
         next(err);
@@ -28,7 +29,7 @@ export const getBattery = async (
 ): Promise<Response | void> => {
     const { battery_id } = req.params;
     try {
-        const battery = await findOne(battery_id);
+        const battery = await findBattery(battery_id);
         return res.status(200).json(battery);
     } catch (err) {
         next(err);
@@ -40,36 +41,45 @@ export const postBattery = async (
     res: Response,
     next: NextFunction
 ): Promise<Response | void> => {
-    const { name, image, price, amperage } = req.body;
-    const newBattery = new Battery();
+    const { name, image, price, amperage, brand, group, user } = req.body;
+    const battery: IBattery = {
+        name,
+        image,
+        price,
+        amperage,
+        stock: 1,
+        brand,
+        group,
+        user,
+        deleted: false,
+    };
     try {
-        newBattery.stock = 1;
-        newBattery.name = name;
-        newBattery.image = image;
-        newBattery.price = price;
-        newBattery.amperage = amperage;
-        const addNew = await create(newBattery);
-        return res.status(200).json(addNew);
+        const newBattery = await createBattery(battery);
+        return res.status(200).json(newBattery);
     } catch (err) {
         next(err);
     }
 };
 
-export const updateBattery = async (
+export const editBattery = async (
     req: Request,
     res: Response,
     next: NextFunction
 ): Promise<Response | void> => {
     const { battery_id } = req.params;
-    const { name, image, price, amperage } = req.body;
+    const { name, image, price, amperage, brand, group, user } = req.body;
+    const updatedBattery: IBattery = {
+        battery_id: Number(battery_id),
+        name,
+        image,
+        price,
+        amperage,
+        brand,
+        group,
+        user,
+    };
     try {
-        const batteryToUpdate = await findOne(battery_id);
-        const updatedBattery: Battery = batteryToUpdate;
-        updatedBattery.name = name;
-        updatedBattery.image = image;
-        updatedBattery.price = price;
-        updatedBattery.amperage = amperage;
-        const updateResults: Battery = await update(updatedBattery);
+        const updateResults: Battery = await updateBattery(updatedBattery);
         return res.status(200).json(updateResults);
     } catch (err) {
         next(err);
@@ -83,7 +93,7 @@ export const deleteBattery = async (
 ): Promise<Response | void> => {
     const { battery_id } = req.params;
     try {
-        const deleteResults = await remove(battery_id);
+        const deleteResults = await removeBattery(battery_id);
         return res.status(200).json(deleteResults);
     } catch (err) {
         next(err);
