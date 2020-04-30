96eb5c146c83fce9326e76446e7a4e63066fdb8c
exroot <eliastorrealba1998@outlook.com>
2020-04-30 13:00:40 -0400

    just renaming

--- backend/src/utils/applyMiddlewares.ts
+++ backend/src/utils/applyMiddlewares.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 type Wrapper = (router: Router) => void;
 
-export const applyMiddleware = (middlewares: Wrapper[], router: Router) => {
+export const applyMiddlewares = (middlewares: Wrapper[], router: Router) => {
     for (const middleware of middlewares) {
         middleware(router);
     }
