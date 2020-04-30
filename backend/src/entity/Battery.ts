ce4635df3f21e4138fe61a68cde7501cf1e1b6e5
exroot <eliastorrealba1998@outlook.com>
2020-04-30 13:03:36 -0400

    adding foreign  keys

--- backend/src/entity/Battery.ts
+++ backend/src/entity/Battery.ts
@@ -5,7 +5,8 @@ import {
     JoinColumn,
     ManyToOne,
 } from "typeorm";
-import { IsNumber, Length } from "class-validator";
+import { IsNumber, Length, IsBoolean } from "class-validator";
+import { Brand, User, Group } from ".";
 
 @Entity("batteries")
 export class Battery {
@@ -32,18 +33,22 @@ export class Battery {
     @IsNumber()
     amperage!: number;
 
-    // @Column()
-    // @ManyToOne((type: Group) => Group, (group: Group) => group.batteries)
-    // @JoinColumn({ name: "group" })
-    // group!: number;
+    @Column()
+    @ManyToOne((type: Group) => Group, (group: Group) => group.batteries)
+    @JoinColumn({ name: "group" })
+    group!: number;
 
-    // @Column()
-    // @ManyToOne((type: Brand) => Brand, (brand: Brand) => brand.batteries)
-    // @JoinColumn({ name: "brand" })
-    // brand!: number;
+    @Column()
+    @ManyToOne((type: Brand) => Brand, (brand: Brand) => brand.batteries)
+    @JoinColumn({ name: "brand" })
+    brand!: number;
 
-    // @Column()
-    // @ManyToOne((type: User) => User, (user: User) => user.batteries)
-    // @JoinColumn({ name: "user" })
-    // user!: number;
+    @Column()
+    @ManyToOne((type: User) => User, (user: User) => user.batteries)
+    @JoinColumn({ name: "user" })
+    user!: number;
+
+    @Column()
+    @IsBoolean()
+    deleted!: boolean;
 }
