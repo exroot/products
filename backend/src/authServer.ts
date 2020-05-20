e491ea1254435102b02383d464f18d9e65183ea6
exroot <eliastorrealba1998@outlook.com>
2020-05-20 11:42:18 -0400

    creating auth server

---
+++ backend/src/authServer.ts
@@ -0,0 +1,50 @@
+import express, { Application } from "express";
+import rateLimit, { RateLimit } from "express-rate-limit";
+import commonMiddlewares from "./middlewares/commons";
+import { errorLogger, errorHandler } from "./middlewares/errorHandlers";
+import authRoutes from "./routes/auth.routes";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+export class AuthServer {
+    protected express: Application;
+    protected apiRateLimiter!: RateLimit;
+
+    constructor() {
+        this.express = express();
+        this.setEnviroment();
+        this.setMiddlewares();
+        this.setRoutes();
+        this.catchErrors();
+    }
+    private setEnviroment(): void {
+        console.log("Setting enviroment...");
+        /* Load .env file and fetched on process.env */
+        dotenv.config();
+        /* Set port on express config */
+        this.express.set("PORT", Number(process.env.AUTH_SERVER_PORT) || 3001);
+        /*  API limiter */
+        this.apiRateLimiter = rateLimit({
+            windowMs: 15 * 60 * 1000, // 15 minutes
+            max: 100,
+        });
+    }
+    private setRoutes(): void {
+        console.log("Setting routes...");
+        this.express.use(authRoutes);
+    }
+    private setMiddlewares(): void {
+        console.log("Loading middlewares...");
+        this.express.use(commonMiddlewares);
+        this.express.use(cookieParser());
+    }
+    private catchErrors(): void {
+        this.express.use(errorLogger);
+        this.express.use(errorHandler);
+    }
+    public listen(): void {
+        const PORT = this.express.get("PORT");
+        this.express.listen(PORT);
+        console.log(`Auth server running on port ${PORT}`);
+    }
+}
