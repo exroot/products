ad7c97995b2983b3b4b7e80d2076bfe9b285a960
exroot <eliastorrealba1998@outlook.com>
2020-05-20 11:42:41 -0400

    adding headers

--- backend/src/middlewares/commons.ts
+++ backend/src/middlewares/commons.ts
@@ -1,7 +1,17 @@
 import morgan from "morgan";
 import cors from "cors";
 import compression from "compression";
-import { urlencoded, json } from "express";
+import { urlencoded, json, Response, Request, NextFunction } from "express";
+
+const headers = (req: Request, res: Response, next: NextFunction) => {
+    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:3000");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+};
 
 export default [
     morgan("dev"),
@@ -9,4 +19,5 @@ export default [
     urlencoded({ extended: false }),
     json(),
     compression(),
+    headers,
 ];
