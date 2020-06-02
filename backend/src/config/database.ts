ed3ee3ba99bbdecc6e81a40184e2d93a526515f7
exroot <eliastorrealba1998@outlook.com>
2020-06-01 22:00:11 -0400

    using enviroment variables on database config file

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,14 +1,17 @@
-import { Battery, Brand, Group, Role, User } from "../entity";
+import { Product, Category, Role, User } from "../entity";
 import { ConnectionOptions } from "typeorm";
+import dotenv from "dotenv";
 import "reflect-metadata";
+
+dotenv.config();
 
 export const dbConfig: ConnectionOptions = {
     type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "exroot",
-    password: "nevera123",
-    database: "getsales",
-    entities: [Battery, Brand, Group, Role, User],
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    entities: [Product, Category, Role, User],
     synchronize: true,
 };
